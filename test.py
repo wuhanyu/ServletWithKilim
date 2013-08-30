0685bc079b6ae7a716ec7512512ea2e42573b15e
wuhanyu <wuhanyu@gmail.com>
2013-08-30 11:22:00 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,13 +1,56 @@
 import os
-n = 5000
+import re
+
+dict = {}
+def setValue(name, concurrentnum, sleeptime, qps, rt):
+    if (not dict.has_key(name)): dict[name] = {}
+    dict[name]["%d:%d:qps" % (concurrentnum, sleeptime)] = qps
+    dict[name]["%d:%d:rt" % (concurrentnum, sleeptime)] = rt
+
+#init
+n = 500
 urllist = [
 'http://localhost:8080/servletcoroutine-0.0.1-SNAPSHOT/sync.html',
 'http://localhost:8080/servletcoroutine-0.0.1-SNAPSHOT/kilim.html'
 ]
 timelist = [100, 200, 500, 1000, 2000, 3000]
+pattern_name = re.compile(r'(\w*).html')
+pattern_qps = re.compile(r'(\d+\d+) [#/sec]')
+pattern_rt = re.compile(r'(\d+\d+) [ms]')
+
+file_object = open('log.txt', 'w')
+
+#begin
+
 for i in range(1, 11):
     for time in timelist:
         for url in urllist:
-            cmd = 'ab -c %d -n %d %s?sleeptime=%d' % ((i * 100), n, url, time)
+            concurrent = i * 100
+            cmd = 'ab -c %d -n %d %s?sleeptime=%d' % (concurrent, n, url, time)
             print cmd
-            os.system(cmd)
+            tmp = "".join(os.popen(cmd).readlines())
+            #print tmp
+            file_object.write(tmp)
+            name =  pattern_name.search(tmp).group(1)
+            qps =  pattern_qps.search(tmp).group(1)
+            rt =  pattern_rt.search(tmp).group(1)
+            setValue(name, concurrent, time, qps, rt)
+            #print dict
+file_object.close()
+            
+for key in dict:
+    print "============%s============" % key
+    print "------------qps------------"
+    for i in range(1, 11):
+        str = ""
+        for time in timelist:
+            concurrent = i * 100
+            str += "%s\t" % dict[key]["%d:%d:qps" % (concurrent, time)]
+        print str
+    print "------------rt------------"
+    for i in range(1, 11):
+        str = ""
+        for time in timelist:
+            concurrent = i * 100
+            str += "%s\t" % dict[key]["%d:%d:rt" % (concurrent, time)]
+        print str
